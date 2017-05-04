522a447b0380e8a5bd61f57f87526d194d01d960
Preda Anuvatnujotikul <preda.anu@gmail>
2017-05-03 21:33:17 -0700

    Saving progress on test file.

--- spec/components/Login.react.test.js
+++ spec/components/Login.react.test.js
@@ -4,16 +4,33 @@ import ReactDOM from 'react-dom';
 import Login from '../../react-client/src/components/Login';
 import renderer from 'react-test-renderer';
 
-describe 
-test('is a stateful class component.', () => {
-  expect(React.Component.isPrototypeOf(Login)).toBe(true);
-});
-
-test('contains an image', () => {
+describe('Login', () => {
   let wrapper = shallow(<Login />);
-  expect(wrapper.contains(<img src="stock_logo.jpg" height="200" width="200"></img>)).toEqual(true);
-})
 
-test('one plus one equals two', () => {
-  expect(1 + 1).toBe(2);
+
+  beforeEach(() => {
+
+  })
+
+  test('is a stateful class component.', () => {
+    expect(React.Component.isPrototypeOf(Login)).toBe(true);
+  });
+
+  test('contains an image', () => {
+    // let wrapper = shallow(<Login />);
+    expect(wrapper.contains(<img src="stock_logo.jpg" height="200" width="200"></img>)).toEqual(true);
+  });
+
+  test('contains a form for entering the users credentials.', () => {
+    // expect(wrapper.contains(<form>)).toEqual(true);
+  });
+
+  test('should update ')
+
+  test('one plus one equals two', () => {
+    expect(1 + 1).toBe(2);
+  });
+
+
 });
+
