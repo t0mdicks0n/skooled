e4bef5bdddc798e4747c1b4d39bf5d431997b871
David Gould <david.gould112@gmail.com>
2017-05-11 10:26:43 -0700

    formats sidebar, installs react-bootstrap and bootstrap

--- services/config/config.js
+++ services/config/config.js
@@ -4,7 +4,9 @@
 // In Heroku set these as environment variables
 // https://devcenter.heroku.com/articles/config-vars
 module.exports = {
+  PASSPORT_JWT_SECRETORKEY : 'tasmanianDevil',
+  MAILGUN_API_KEY : 'key-50bed3008d8fd0cbba4542fef47c99da',
+  MAILGUN_DOMAIN : 'sandbox7a00893cedda4c6b9e571b38f2f633d6.mailgun.org',
   STRIPE_PUBLISHABLE_KEY : 'pk_test_jKhrDXJx4xcBCslMg3Nn4pWD',
   STRIPE_SECRET_KEY : 'sk_test_vYCmot5s3142IDIM3fZgplYf',
-  PASSPORT_JWT_SECRETORKEY : 'tasmanianDevil'
 };
