9ec67a7ce30176542b3d187fa3541b45e8b32f35
David Gould <david.gould112@gmail.com>
2017-05-10 19:10:35 -0700

    adds Nick's config.js from Google Drive

---
+++ services/config/config.js
@@ -0,0 +1,10 @@
+// Remember to set your service keys below
+// Save this file as 'config.js' to use locally
+
+// In Heroku set these as environment variables
+// https://devcenter.heroku.com/articles/config-vars
+module.exports = {
+  STRIPE_PUBLISHABLE_KEY : 'pk_test_jKhrDXJx4xcBCslMg3Nn4pWD',
+  STRIPE_SECRET_KEY : 'sk_test_vYCmot5s3142IDIM3fZgplYf',
+  PASSPORT_JWT_SECRETORKEY : 'tasmanianDevil'
+};
