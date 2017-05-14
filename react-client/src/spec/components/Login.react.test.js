9edcb73cbadd8cabd5688bb1ec88e7c24ec83c54
Preda Anuvatnujotikul <preda.anu@gmail>
2017-05-13 18:51:33 -0700

    Written front end jest tests for Document component.

--- react-client/src/spec/components/Login.react.test.js
+++ react-client/src/spec/components/Login.react.test.js
@@ -1,11 +1,12 @@
 import { shallow } from 'enzyme';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
 import Login from '../../components/Login';
 import renderer from 'react-test-renderer';
 
 describe('Login', () => {
-  // let wrapper = shallow(<Login />);
+  let wrapper = shallow(<Login />);
 
 
   test('is a stateful class component.', () => {
@@ -29,6 +30,11 @@ describe('Login', () => {
     expect(1 + 1).toBe(2);
   });
 
+  test('invokes handleSubmit method when button is clicked', () => {
+    const login = shallow(<Login />)
+  })
+
 
 });
 
+it('Login component contains an input form')
